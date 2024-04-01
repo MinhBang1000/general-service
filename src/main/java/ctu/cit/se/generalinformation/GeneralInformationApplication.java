767fdc5fa36e6e1197481014927c252985367491
Minh Bang <bradonleminhbang1@gmail.com>
2024-04-01 08:05:16 +0700

    First commit

---
+++ src/main/java/ctu/cit/se/generalinformation/GeneralInformationApplication.java
@@ -0,0 +1,13 @@
+package ctu.cit.se.generalinformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralInformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ctu/cit/se/generalinformation/GeneralInformationApplicationTests.java
@@ -0,0 +1,13 @@
+package ctu.cit.se.generalinformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralInformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

f463120a6efb1f173f20ac9a594ee5e53b104f60
Minh Bang <bradonleminhbang1@gmail.com>
2024-04-01 08:33:25 +0700

    GI_0001: Creating the repositories for all entities

---
+++ src/main/java/ctu/cit/se/generalinformation/repositories/IProjectRepository.java
@@ -0,0 +1,12 @@
+package ctu.cit.se.generalinformation.repositories;
+
+import ctu.cit.se.generalinformation.entities.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface IProjectRepository extends JpaRepository<Project, UUID> {
+
+}
